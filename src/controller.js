b8f4b43b59d199ddf45f9e0be5078c81e1b6e4f0
Jennifer Cant <jcant6@gmail.com>
2020-11-24 14:25:51 +0000

    added ship

--- src/controller.js
+++ src/controller.js
@@ -30,6 +30,13 @@
         portsElement.style.width = `${portsElementWidth + 256}px`;
       });
     }
+    renderShips(ship){
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+      const shipElement = document.querySelector("#ship");
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+    }
   }
 
   if (typeof module !== "undefined" && module.exports) {
